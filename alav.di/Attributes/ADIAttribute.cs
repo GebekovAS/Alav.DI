aeb9020848500e9cc038ad1074603b9dc1ba6cec
Алавутдин Гебеков <gebekovas@mail.ru>
2022-10-08 11:06:51 +0300

    Add comments

--- alav.di/Attributes/ADIAttribute.cs
+++ alav.di/Attributes/ADIAttribute.cs
@@ -5,10 +5,19 @@ using System.Text;
 
 namespace Alav.DI.Attributes
 {
+    /// <summary>
+    /// Attribute for class inclusion in scan results
+    /// </summary>
     public class ADIAttribute: Attribute
     {
+        /// <summary>
+        /// Service lifetime (Singleton, Transient ...)
+        /// </summary>
         public ADIServiceLifetime ServiceLifetime { get; set; }
 
+        /// <summary>
+        /// Service interface (DI)
+        /// </summary>
         public Type? Interface { get; set; }
     }
 }
